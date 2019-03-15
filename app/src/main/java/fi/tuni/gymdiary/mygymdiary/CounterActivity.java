58c64e3b97e53d1daadd76b93cdb84adbc964c77
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-15 18:03:14 +0200

    Add: new activities

---
+++ app/src/main/java/fi/tuni/gymdiary/mygymdiary/CounterActivity.java
@@ -0,0 +1,6 @@
+package fi.tuni.gymdiary.mygymdiary;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class CounterActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/fi/tuni/gymdiary/mygymdiary/ExerciseActivity.java
@@ -0,0 +1,15 @@
+package fi.tuni.gymdiary.mygymdiary;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class ExerciseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise);
+
+    }
+}
