a70a6f93eb0339e784a5f0b18bbe1e7daa60993d
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-15 18:03:38 +0200

    Add: buttonClicked()

--- app/src/main/java/fi/tuni/gymdiary/mygymdiary/MainActivity.java
+++ app/src/main/java/fi/tuni/gymdiary/mygymdiary/MainActivity.java
@@ -1,7 +1,10 @@
 package fi.tuni.gymdiary.mygymdiary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void buttonClicked(View view) {
+        if(view.getId() == R.id.exercises) {
+            Intent intent = new Intent(this, ExerciseActivity.class);
+            startActivity(intent);
+        }
     }
 }
