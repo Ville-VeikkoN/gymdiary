00558cb455a8d187e667216558981c8bc195483d
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-19 13:04:50 +0200

    Add: Variable for table columns and creating them

--- app/src/main/java/fi/tuni/gymdiary/mygymdiary/MyDbHelper.java
+++ app/src/main/java/fi/tuni/gymdiary/mygymdiary/MyDbHelper.java
@@ -6,6 +6,35 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class MyDbHelper extends SQLiteOpenHelper {
 
+    //Table names
+    private static final String WEIGHT_TABLE = "weight";
+    private static final String EXERCISE_TABLE = "exercise";
+
+    //Common columns name
+    private static final String KEY_ID = "ID";
+    private static final String KEY_DATE = "date";
+
+    //Weight table- columns
+    private static final String KEY_WEIGHT = "weight";
+
+    //Exercise table- columns
+    private static final String KEY_ACTIVITY = "activity";
+    private static final String KEY_SETS = "sets";
+    private static final String KEY_REPS = "reps";
+    private static final String KEY_WEIGHTAMOUNT = "weight";
+
+    //Create table statement for weight
+    private static final String CREATE_TABLE_WEIGHT = "CREATE TABLE "
+            + WEIGHT_TABLE + "(" + KEY_ID + " INTEGER PRIMARY KEY," + KEY_DATE
+            + " DATE," + KEY_WEIGHT + " REAL"  + ")";
+
+    //Create table statement for exercise
+    private static final String CREATE_TABLE_EXERCISE = "CREATE TABLE "
+            + EXERCISE_TABLE + "(" + KEY_ID + " INTEGER PRIMARY KEY," + KEY_DATE
+            + " DATE," + KEY_ACTIVITY + " TEXT," + KEY_SETS +"INTEGER,"
+            + KEY_REPS + "INTEGER," + KEY_WEIGHTAMOUNT + "REAL" + ")";
+
+
     public MyDbHelper(Context context){
         super(context,"tableName",null,1);
     };
@@ -17,6 +46,14 @@ public class MyDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void createWeightTable() {
+
+    }
+
+    public void createExercisesTable() {
 
     }
 }
